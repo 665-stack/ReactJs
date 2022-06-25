dc836f1798dec2dc57251a8f694b7c8b437b8613
unknown <mahmodulshahin665@gmail.com>
2022-06-25 12:37:06 +0600

    React Extension Pack and component with css file

--- react-rest-countries/src/App.js
+++ react-rest-countries/src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
-function LoadCountries() {
+
+
+
+
+
+
+
+/* function LoadCountries() {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -20,8 +32,20 @@ function LoadCountries() {
     <div>
       <h1>Visinting every country of the world.</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+
+      }
     </div>
   )
 }
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+} */
 
 export default App;
