539252aecb96d3784f7e5e3919a8a05df2c442f0
unknown <mahmodulshahin665@gmail.com>
2022-06-28 20:30:37 +0600

    Load data using the useEffect and display data

--- reactjs-core-concepts-recap/src/App.js
+++ reactjs-core-concepts-recap/src/App.js
@@ -5,18 +5,48 @@ import React, { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
+      <LoadPost></LoadPost>
       <District name="Gazipur" spacial="Industrial city"></District>
       <District name="Dhaka" spacial="Capital city"></District>
       <District name="Kishorganj" spacial="Haor"></District>
     </div>
   );
 }
+// loadPost component
+function LoadPost() {
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setPost(data))
+  }, [])
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+// post component
+function Post(props) {
+  return (
+    <div className='post-container'>
+      <h3>Title: {props.title}</h3>
+      <p>Body: {props.body}</p>
+    </div>
+  )
+}
+
+// object for style
 const districtStyle = {
   backgroundColor: 'brown',
   margin: '20px',
   borderRadius: '20px',
   padding: '20px'
 }
+// district component
 function District(props) {
   const [power, setPower] = useState(1);
 
