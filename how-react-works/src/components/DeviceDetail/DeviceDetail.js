ea0142a99053c902f019dff61f38692ad2b4754f
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-07 22:29:10 +0600

    practice lots of stuff

---
+++ how-react-works/src/components/DeviceDetail/DeviceDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeviceDetail = (props) => {
+
+    return (
+        <div>
+            <h5>price: {props.price}</h5>
+        </div>
+    );
+};
+
+export default DeviceDetail;
---
+++ how-react-works/src/components/watch/Watch.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Display from '../Display/Display';
+
+const Watch = () => {
+    const [steps, setSteps] = useState(0);
+
+    const increaseSteps = () => {
+        const newStepsCount = steps + 1;
+        setSteps(newStepsCount);
+
+    }
+    useEffect(() => {
+        console.log(steps);
+    }, [steps])
+
+    return (
+        <div style={{ border: '2px solid black', margin: '20px' }}>
+            <h2>This is my smart watch</h2>
+            <h3>My current steps: {steps}</h3>
+            <button onClick={increaseSteps}>Steps</button>
+            <Display name={'garmin'} steps={steps}></Display>
+        </div>
+    );
+};
+
+export default Watch;
