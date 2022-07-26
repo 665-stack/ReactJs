16593312b34e4c8feb6de445618eaaf86eb9d750
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-26 13:49:05 +0600

    existing card component  is created

--- ema-john-simple-2/src/Components/Shop/Shop.js
+++ ema-john-simple-2/src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -30,19 +31,7 @@ const Shop = () => {
             </div>
             {/* cart section */}
             <div className='cart-container'>
-                <h5>Our Summary</h5>
-                <div className='order-details'>
-                    <p>Selected items: {cart.length}</p>
-                    <p>Total Price: $</p>
-                    <p>Total Shipping Charge: $</p>
-                    <p>Tax: $</p>
-                    <h6>Grand Total: $</h6>
-                </div>
-                <div className='btns'>
-                    <button className='clear-cart-btn'><p>Clear Cart</p></button>
-                    <br />
-                    <button className='review-order-btn'><p>Review Order</p></button>
-                </div>
+                <Cart cart={cart}></Cart>
             </div>
 
         </div>
