3f22b4a234dc7f40889a65f8353cf7cfa6db601b
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-31 15:14:08 +0600

    object des. & array des.

--- react spa/destrutuing.js
+++ react spa/destrutuing.js
@@ -6,7 +6,7 @@ const personObject = {
 const cart = { abc: 1, def: 2, ghi: 3 };
 const personArray = ['Mahmodul', 'Shahin'];
 // Object dot notation and square bracket notation
-// Dot notation
+/* // Dot notation
 const firstName = personObject.firstName;
 console.log(firstName);
 
@@ -20,9 +20,28 @@ console.log(name);
 const giveQuantity = (id) => {
     console.log(cart[id]);
 }
-giveQuantity('abc')
+giveQuantity('abc') */
 
 // Simple Object Destructuing
+// const { firstName, lastName, address } = personObject;
+// console.log(firstName, address);
+
+// Array index
+// const firstName = personArray[0];
+
+// Array Destruturing
+const [firstName, lastName] = personArray;
+// but when I dont need firstName then what should I do. I need use ',' for firstName. Example: --
+const personFav = ['Black', 'LofiSongs', 'MidNight', 'Tech'];
+// suppose here I need MidNight from personFav array.
+const [, , MidNight] = personFav;
+console.log(MidNight);
+
+// Object Destruturing Pro tricks
+
+
+
+
 
 
 
