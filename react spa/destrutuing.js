ceecca3a28ba9423325780676c7ae4dbe33ca323
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-07-31 15:59:13 +0600

    multi level des., default value, re-assigning

--- react spa/destrutuing.js
+++ react spa/destrutuing.js
@@ -1,17 +1,18 @@
 const personObject = {
     firstName: 'Mahmodul',
     lastName: 'Shahin',
+    age: 18,
     address: { country: 'Bangladesh', capital: 'Dhaka' }
 };
 const cart = { abc: 1, def: 2, ghi: 3 };
 const personArray = ['Mahmodul', 'Shahin'];
 // Object dot notation and square bracket notation
-/* // Dot notation
+/* // ========= Dot notation ========
 const firstName = personObject.firstName;
 console.log(firstName);
 
 
-// Square Bracket Notation
+// ======= Square Bracket Notation =========
 const lastName = ['lastName']
 
 const name = personObject[lastName];
@@ -22,14 +23,14 @@ const giveQuantity = (id) => {
 }
 giveQuantity('abc') */
 
-// Simple Object Destructuing
+// ======== Simple Object Destructuing =========
 // const { firstName, lastName, address } = personObject;
 // console.log(firstName, address);
 
-// Array index
+// ===== Array index ======
 // const firstName = personArray[0];
 
-// Array Destruturing
+// ===== Array Destruturing ====== 
 const [firstName, lastName] = personArray;
 // but when I dont need firstName then what should I do. I need use ',' for firstName. Example: --
 const personFav = ['Black', 'LofiSongs', 'MidNight', 'Tech'];
@@ -37,7 +38,48 @@ const personFav = ['Black', 'LofiSongs', 'MidNight', 'Tech'];
 const [, , MidNight] = personFav;
 console.log(MidNight);
 
-// Object Destruturing Pro tricks
+// ======= Object Destruturing Pro tricks ======
+// Multi level des.
+const company = {
+    ceo: 'Irfan Islam',
+    section: {
+        tech: {
+            usesTechnology: ['html', 'css', 'js', 'react'],
+            averageSalary: 30000,
+            manPower: { frontEnd: 'Shahin', backEnd: 'Nirob' }
+
+        },
+        product: {
+            averageSalary: 20000,
+            manPower: { accounted: 'Shihab', manager: 'Nayeem' }
+        }
+    },
+    CompanyAddress: { country: 'india', city: 'mumbai' }
+};
+const { frontEnd, backEnd } = company.section.tech.manPower;
+console.log(frontEnd, backEnd);
+//---
+const { CompanyAddress: { country, city } } = company;
+console.log(country, city);
+
+//  ====== Set Default Value: jodi object e data na pay taile ei default value ta dekhabe. ar thakle dekhabe na. simple :)
+const { age = 'Not Available' } = personObject;
+console.log(age);
+
+
+// ==== re-assigning ==== 
+// oporer 'company' namer object eo 'country' namer akte var destructuring kora hoice. abong akaner 'wifi' namer object thekeo 'country' namer var destructuring kora hoice. But akhane akta clash lege jabe. jar fole error asbe. r tai 'wifi' object er 'country' ke re-assign kore 'location' kore deya hoice.
+const wife = {
+    name: 'name janina',
+    age: 18,
+    hight: "5 feet 3 inch",
+    weight: '48 kg',
+    country: 'Bangladesh'
+};
+const { country: location } = wife;
+console.log(location);
+
+
 
 
 
